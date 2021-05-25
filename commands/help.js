c4f199f43b7c1a06566bd58686138ee53335b1ab
vangod <vachri.attala@gmail.com>
2021-05-25 18:31:45 +0700

    format code

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,39 @@
-const { prefix } = require('../config.json');
+const { prefix } = require("../config.json");
 
 module.exports = {
-	name: 'help',
-	description: 'Display command help information',
-    aliases: [],
-    usage: '<command name>\n!help',
-	execute(message, args) {
-        const data = [];
-        const { commands } = message.client;
-        if(!args.length){
-            message.channel.send("```Heres the list of my commands\n\n" + `${commands.map(command => command.name).join(', ')}` + "\n !help <command name>```");
-        }else{
-            const name = args[0].toLowerCase();
-            const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+  name: "help",
+  description: "Display command help information",
+  aliases: [],
+  usage: "<command name>\n!help",
+  execute(message, args) {
+    const data = [];
+    const { commands } = message.client;
+    if (!args.length) {
+      message.channel.send(
+        "```Heres the list of my commands\n\n" +
+          `${commands.map((command) => command.name).join(", ")}` +
+          "\n !help <command name>```"
+      );
+    } else {
+      const name = args[0].toLowerCase();
+      const command =
+        commands.get(name) ||
+        commands.find((c) => c.aliases && c.aliases.includes(name));
 
-            if (!command) {
-                return message.reply('that\'s not a valid command!');
-            }
+      if (!command) {
+        return message.reply("that's not a valid command!");
+      }
 
-            data.push(`**Name:** ${command.name}`);
+      data.push(`**Name:** ${command.name}`);
 
-            if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-            if (command.description) data.push(`**Description:** ${command.description}`);
-            if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+      if (command.aliases)
+        data.push(`**Aliases:** ${command.aliases.join(", ")}`);
+      if (command.description)
+        data.push(`**Description:** ${command.description}`);
+      if (command.usage)
+        data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
-            message.channel.send(data, { split: true });
-        }
-	},
+      message.channel.send(data, { split: true });
+    }
+  },
 };
