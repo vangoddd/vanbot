fcd23f7003e756269ffd53fab6daf2e9e1d369c0
vangoddd <vachri.attala@gmail.com>
2021-05-25 14:20:23 +0700

    installed pixiv api

---
+++ commands/pixiv.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "pixiv",
+  description: "Sends you a pixiv illustration",
+  usage: "<tag>",
+  execute(message, args) {
+    console.log("Pixiv command executed");
+  },
+};
