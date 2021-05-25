b7cf423ab4ea330f048670fc8091911ef322de09
vangoddd <vachri.attala@gmail.com>
2021-05-25 14:36:33 +0700

    fix dir error in pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -1,21 +1,27 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-	name: 'pp',
-	description: 'Display pp of an osu user',
-    usage: '<osu username>',
-	execute(message, args) {
-        const osuKey = JSON.parse(fs.readFileSync('../osuKey.json', 'utf8'));
-        const apiURL = 'https://osu.ppy.sh/api/v2/users/' + args[0];
-        const AuthStr = "Bearer "+osuKey.access_token;
-        axios.get(apiURL, { headers: { Authorization: AuthStr } })
-        .then(response => {
-            // console.log(response);
-            message.channel.send(`This osu user's pp is: ${response.data.statistics.pp}`);
-        })
-        .catch((error) => {
-            console.log('error');
-        });
-	},
+  name: "pp",
+  description: "Display pp of an osu user",
+  usage: "<osu username>",
+  execute(message, args) {
+    const osuKey = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, "../osuKey.json"), "utf8")
+    );
+    const apiURL = "https://osu.ppy.sh/api/v2/users/" + args[0];
+    const AuthStr = "Bearer " + osuKey.access_token;
+    axios
+      .get(apiURL, { headers: { Authorization: AuthStr } })
+      .then((response) => {
+        // console.log(response);
+        message.channel.send(
+          `This osu user's pp is: ${response.data.statistics.pp}`
+        );
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  },
 };
